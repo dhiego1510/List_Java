b9f4225dc25880a0bbc335aaddee7f83a054c92c
Diego <dhiego1510@gmail.com>
2024-05-09 14:40:10 -0500

    created menu

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,34 +1,34 @@
-import java.util.ArrayList;
-import java.util.List;
+  import java.util.ArrayList;
+  import java.util.List;
 
-public class CreditCard {
-  private double limit;
-  private double balance;
-  private List<Buys> shoppinglist;
+  public class CreditCard {
+    private double limit;
+    private double balance;
+    private List<Buys> shoppinglist;
 
-  public CreditCard(double limit ) {
-    this.limit = limit;
-    this.balance = balance;
-    this.shoppinglist = new ArrayList<>();
-  }
-  public boolean checkBalance(Buys buy) {
-    if(this.balance >= buy.getValue()) {
-      this.balance -= buy.getValue();
-      this.shoppinglist.add(buy);
-      return true;
+    public CreditCard(double limit ) {
+      this.limit = limit;
+      this.balance = limit;
+      this.shoppinglist = new ArrayList<>();
     }
-    return false;
-  }
+    public boolean checkBalance(Buys buy) {
+      if(this.balance >= buy.getValue()) {
+        this.balance -= buy.getValue();
+        this.shoppinglist.add(buy);
+        return true;
+      }
+      return false;
+    }
 
-  public double getLimit() {
-    return limit;
-  }
+    public double getLimit() {
+      return limit;
+    }
 
-  public double getBalance() {
-    return balance;
-  }
+    public double getBalance() {
+      return balance;
+    }
 
-  public List<Buys> getShoppinglist() {
-    return shoppinglist;
+    public List<Buys> getShoppinglist() {
+      return shoppinglist;
+    }
   }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
   public static void main(String[] args) {
+
+    Scanner lecture = new Scanner(System.in);
+    System.out.println("Escriba el límite de la tarjeta: ");
+    double limit = lecture.nextDouble();
+    CreditCard card = new CreditCard(limit);
+
+    int exit = 1;
+    while(exit != 0) {
+      System.out.println("Escriba la descripción de la compra:");
+      String description = lecture.next();
+
+      System.out.println("Escriba el valor de la compra:");
+      double value = Double.valueOf(lecture.next());
+
+      Buys buy = new Buys(value, description);
+      boolean buyCompleted = card.checkBalance(buy);
+
+      if (buyCompleted) {
+        System.out.println("Compra realizada!");
+        System.out.println("Escriba 0 para salir o 1 para continuar");
+        exit = lecture.nextInt();
+      } else {
+        System.out.println("Saldo insuficiente!");
+        exit = 0;
+      }
+    }
+    System.out.println("***********************");
+    System.out.println("COMPRAS REALIZADAS:\n");
+    for (Buys buy : card.getShoppinglist()) {
+      System.out.println(buy.getDescription() + " - " +buy.getValue());
+    }
+    System.out.println("\n***********************");
+    System.out.println("\nSaldo de la tarjeta: " +card.getBalance());
+
   }
 }
